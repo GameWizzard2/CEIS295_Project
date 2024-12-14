5f61cc093a6da00e3c1fe4346b6c12e52ea7d41a
GameWizzard2 <98629019+GameWizzard2@users.noreply.github.com>
2024-12-14 18:18:23 -0500

    Reformated module 2, seperated some functions from LinkedListActualSpeed and place them in either, main.py or utilities.py

--- Module_2/LinkedListActualSpeed.py
+++ Module_2/LinkedListActualSpeed.py
@@ -1,43 +1,20 @@
 # Name: Christopher H Barfield
 # Date: 2024-11-07
 
-import sys
+# local
+from LinkedList import LinkedList
+from Client import Client
+from utilities import (
+     appendToLinkedList,
+)
+
+# Built-in
+from datetime import date
 import random
 import time
 
-from datetime import date
-from LinkedList import LinkedList
-from Client import Client
+# Imported
 from typing import List
-
-def createClientRecords(newList = []):
-    input_file_name = '.\Module_2\ClientData.csv'
-    with open(input_file_name) as infile:
-        for line in infile:
-            s = line.split(',')
-            client_id = int(s[0])  # convert the default string to an int
-            f_name = s[1]
-            l_name = s[2]
-            phone = s[3]
-            email = s[4]
-            
-            # create a client object using the data from the file
-            clt = Client(client_id, f_name, l_name, phone, email)
-            
-            # add the client object to the list
-            newList.append(clt)
-        return newList
-
-def appendToLinkedList(numofClients: int, linkedList: 'LinkedList', clientRecords: List['Client']):
-    """
-    Add clientRecords to the end of a linked list.
-    """
-    for i in range(numofClients):
-        linkedList.add_last(clientRecords[i])
-
-def userContinueInput():
-    print("Press \"Enter\" to continue...")
-    sys.stdin.read(1)  # Reads one character (like Enter) from standard input
 
 
 def testLinkedOne(numofClients: int, linkedList: 'linkedList', clientRecords: List['Client']):
@@ -141,18 +118,3 @@ def testLinkedThree(numofClients: int, linkedList: 'LinkedList', clientRecords: 
     endTime = time.time()
     elapsedTime = endTime - startTime
     print(f"\n3.\tTime taken to add {numofClients}, and then display and remove 1000 random client records: {elapsedTime:.6f} seconds")
-
-print("Name:", "Christopher H Barfield")
-print("Date:", date.today())
-clientRecords = createClientRecords()
-numofClients = len(clientRecords)
-funWithLinkedLists = LinkedList()
-
-testLinkedOne(numofClients, funWithLinkedLists, clientRecords)
-testLinkedOneContinued(numofClients, funWithLinkedLists)
-userContinueInput()
-appendToLinkedList(numofClients, funWithLinkedLists, clientRecords)
-testLinkedTwo(numofClients, funWithLinkedLists)
-userContinueInput()
-testLinkedThree(numofClients, funWithLinkedLists, clientRecords)
-userContinueInput()
