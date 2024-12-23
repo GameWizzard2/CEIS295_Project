68aee5b938fb3182beeb0e2d463e3437dafb3e8b
GameWizzard2 <98629019+GameWizzard2@users.noreply.github.com>
2024-12-22 21:21:25 -0500

    refromatted folder structure

--- Module_2/Node.py
+++
@@ -1,7 +0,0 @@
-#Name: Christopher Barfield
-#Date: 11/7/24
-
-class Node:
-    def __init__(self, initial_data=None):
-        self.data = initial_data
-        self.next = None
--- Module_3/Node.py
+++
@@ -1,7 +0,0 @@
-# Name: Christopher H Barfield
-# Date: 2024-16-07
-
-class Node:
-    def __init__(self, initial_data=None):
-        self.data = initial_data
-        self.next = None
